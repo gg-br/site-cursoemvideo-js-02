e5d07e27e7d5e552de612f1b6119d5a607bb1caa
ggfbrasil <165701240+ggfbrasil@users.noreply.github.com>
2024-04-25 12:25:22 -0300

    25/04/24

---
+++ script.js
@@ -0,0 +1,77 @@
+function verificar ()
+{
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('ano')
+    var res = document.getElementById('res')
+
+    if (fano.value.length == 0 || Number(fano.value) > ano)
+    {
+        window.alert('erro')
+    }
+
+    else
+    {
+        var fsex = document.getElementsByName('sexo')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked)
+        {
+            genero = 'Homem'
+
+            if (idade >=0 && idade < 10)
+            {
+                img.setAttribute('src', 'imagens/hbebe.jpg')
+            }
+
+            else if (idade <21)
+            {
+                img.setAttribute('src', 'imagens/hjovem.jpg')
+            }
+
+            else if (idade <50)
+            {
+                img.setAttribute('src', 'imagens/h.jpg')
+            }
+
+            else
+            {
+                img.setAttribute('src', 'imagens/hvelho.jpg')
+            }     
+        }
+
+        else if (fsex[1].checked)
+        {
+            genero = 'Mulher'
+
+            if (idade >=0 && idade < 10)
+            {
+                //kid
+            }
+
+            else if (idade <21)
+            {
+                //jovem
+            }
+
+            else if (idade <50)
+            {
+                //adulto
+            }
+
+            else
+            {
+                //idoso
+            }     
+        }
+        
+        res.style.textAlign = 'center'
+        res.innerHTML = `${genero} com ${idade} anos`
+        res.appendChild(img)
+
+    }
+}
